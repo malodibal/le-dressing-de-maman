b5faf95e916495a0c85a558df7c33a78ab6f00d4
malodibal <melodiebalfroid@outlook.fr>
2026-05-17 16:52:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,32 @@
-const CACHE = 'dressing-v1';
-const FILES = ['/'];
+const CACHE = 'garde-robe-v8';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(FILES);
-    })
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k){ return k !== CACHE; })
-            .map(function(k){ return caches.delete(k); })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(cached) {
-      return cached || fetch(e.request).catch(function() { return cached; });
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('/index.html'));
     })
   );
 });
